48d4b1feb376a127dcafb578b72d0c5615d11da3
Helena Havelkova <hhavelkova@localhost.localdomain>
2017-05-31 10:56:05 -0400

    Prvni commit

---
+++ src/verzeWildfly.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#set -x
+
+if [ -e $2 ]
+then 
+	echo -e "\e[92mTed se zjistuje, jaka je aktualni verze wildfly.\e[0m" 
+else
+	echo -e "\e[96mZadej spravnou slozku!\e[0m"
+	exit 
+fi
+
+
+if ! [ -e $2/wildfly ] 
+then
+     cd $2 		
+     wget http://download.jboss.org/wildfly/$1/wildfly-$1.zip 2> /dev/null
+
+	if ! [ $? == 0 ]
+	  then  
+		echo -e "\e[96mZadej spravnou verzi!\e[0m"
+		exit
+	fi  
+
+     unzip -q wildfly-$1.zip -d wildfly 
+    
+fi
+
+cd $2/wildfly/wildfly-$1/   
+./bin/standalone.sh > /dev/null &
+sleep 20s
+
+zabit=`jps | grep "jboss" | sed "s/jboss.*//" `
+kill $zabit
+#pwd
+grep "services are lazy, passive or on-demand" standalone/log/server.log | tail -1 | sed 's/.* Full //' | sed "s/ (.*//"
+
+cd ../..  
+#nebo $2
+rm -rf wildfly
+rm -rf wildfly-$1.zip
+
+
---
+++ test/chybicky.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#set -x
+
+cd ../src
+echo -e "\e[105mTed se dejou kouzla
+\e[0m"
+
+overovani () 
+{
+cd ../vystup
+
+rozdily1=$(diff standardniVystup.txt vzor1.txt) 
+if [ "$rozdily1" = "" ] 
+then
+    echo -e "\e[92mVse bezi spravne.\e[0m"
+
+else 
+    echo -e "\e[91mVe standardnim vystupu je neco navic.\e[0m"
+fi
+
+rozdily2=$(diff chybovyVystup.txt vzor2.txt) 
+if [ "$rozdily2" = "" ] 
+then
+    echo -e "\e[92mV chybovem vzstupu jsem nenasla zadnou chybu.\e[0m"
+else 
+    echo -e "\e[91mChybovy vystup hlasi chybu.\e[0m"
+fi
+
+cd ../src
+}
+
+#vse by melo bezet spravne
+echo -e "\e[96mPrvni test\e[0m"
+./verzeWildfly.sh 10.1.0.Final ../vystup > ../vystup/standardniVystup.txt 2> ../vystup/chybovyVystup.txt
+
+overovani
+
+#je zadana spatna verze
+echo -e "\e[96mDruhy test\e[0m"
+./verzeWildfly.sh 10.1.0.F ../vystup > ../vystup/standardniVystup.txt 2> ../vystup/chybovyVystup.txt
+
+overovani
+
+#je zadano chybne umisteni
+echo -e "\e[96mTreti test\e[0m"
+./verzeWildfly.sh 10.1.0.Final vystup > ../vystup/standardniVystup.txt 2> ../vystup/chybovyVystup.txt
+
+overovani
+
+cd ../vystup
+rm -rf standardniVystup.txt
+rm -rf chybovyVystup.txt
+
+#co jsem se naucila nebo taky ne
+
+#echo "zdravim"  2>&1 | tee ~/VerzeWildfly/vystup.txt
+#open vystup.txt /  gedit
+
